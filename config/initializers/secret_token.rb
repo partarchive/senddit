55b064d992be5eacbdc3fbb5dea2f0867216c934
Kyle Partridge <partkyle@gmail.com>
2011-12-14 13:56:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Senddit::Application.config.secret_token = 'a9f92481899b7cbb7b100e97d5628b115d7dddf50b0f55e4a87eb0a33c6eca947ed6204971701b0f8e1191bf33dbef73b2f23c127c7e97c0e01b56f889e271c1'

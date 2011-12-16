bd6e26b5b39492a6a1f305777fa458286dd0e0b3
Jorge Urias <jorge.urias@sendgrid.com>
2011-12-16 07:53:49 -0600

    Fixed all the tests

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -4,8 +4,11 @@ describe "Comments" do
   describe "GET /comments" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get comments_path
-      response.status.should be(200)
+      post = create(:post)
+      comment = build(:comment)
+      post post_comments_path(post), comment: comment.attributes
+
+      response.should redirect_to(post)
     end
   end
 end

5d10626d777809472ba892dc61debf5e9dd52812
David Tomberlin <david.tomberlin@sendgrid.com>
2011-12-29 11:25:37 -0800

    added simple functional test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe "Posts" do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get posts_path
-      response.status.should be(200)
+    it "displays posts" do
+      create(:post, title: "Moo title")
+      visit posts_path
+      page.should have_content("Moo title")
+    end
+  end
+
+  describe "POST /posts" do
+    it "create post" do
+      visit new_post_path
+      fill_in "post_title",  :with => "Moo title"
+      fill_in "post_link",   :with => "http:moo.com"
+      fill_in "post_email",  :with => "moo@moo.com"
+      click_button "Create Post"
+      page.should have_content("Post was successfully created.")
+      page.should have_content("Moo title")
     end
   end
 end

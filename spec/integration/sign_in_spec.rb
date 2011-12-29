27eb9076c43bacd2a84e66806a8891e77f4ac752
CJ Buchmann <cj.buchmann@gmail.com>
2011-12-29 15:08:50 -0800

    Added user sign_in integration tests and also added Aptana Project ignore line.

--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -7,13 +7,62 @@ describe 'home page' do
     user = create(:user)
     
     fill_in('user[email]', :with => user.email)
-    fill_in('user[password]', :with => user.password)
+    fill_in('user[password]', :with => 'this_is_not_a_valid_password')
     click_button('header_sign_in')
     
     current_path.should == '/users/sign_in'
   end
   
   it 'logs in using valid credentials' do
+    visit '/'
     
+    user = create(:user)
+    
+    fill_in('user[email]', :with => user.email)
+    fill_in('user[password]', :with => user.password)
+    click_button('header_sign_in')
+    
+    current_path.should == '/'
+    page.should have_content(user.email)
+  end
+  
+  it 'logs in with invalid credentails and then logs into /users/sign_in with invalid credentials' do
+    visit '/'
+    
+    user = create(:user)
+    
+    fill_in('user[email]', :with => user.email)
+    fill_in('user[password]', :with => 'this_is_not_a_valid_password')
+    click_button('header_sign_in')
+    
+    current_path.should == '/users/sign_in'
+    
+    within("form[@id='user_new']") do
+      fill_in('user[email]', :with => 'testing')
+      fill_in('user[password]', :with => 'testing')
+      click_button('Sign in')
+    end
+    
+    current_path.should == '/users/sign_in'
+  end
+  
+  it 'logs in with invalid credentails and then logs into /users/sign_in with valid credentials' do
+    visit '/'
+    
+    user = create(:user)
+    
+    fill_in('user[email]', :with => user.email)
+    fill_in('user[password]', :with => 'this_is_not_a_valid_password')
+    click_button('header_sign_in')
+    
+    current_path.should == '/users/sign_in'
+    
+    within("form[@id='user_new']") do
+      fill_in('user[email]', :with => user.email)
+      fill_in('user[password]', :with => user.password)
+      click_button('Sign in')
+    end
+    
+    current_path.should == '/'
   end
 end

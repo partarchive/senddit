39728be404ca43744cc29c8a253aa957d0de4dfc
Kyle Partridge <partkyle@gmail.com>
2011-12-28 16:10:26 -0800

    created some basic tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
+    email "test@example.com"
+    password "secret"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should require an email address" do
+    user = build(:user, email: nil)
+    user.should_not be_valid
+  end
+
+  it "should only allow a valid email" do
+    user = build(:user, email: "bad/email")
+    user.should_not be_valid
+  end
+
+  it "should require a password" do
+    user = build(:user, password: nil)
+    user.should_not be_valid
+  end
+
 end
